31b157b6bb6a1e07b73428d042d17744f2b8f736
Sergey Brendyukov <topdev@askfirst.space>
2019-12-26 23:05:37 +0300

    Updated session

--- src/session.ts
+++ src/session.ts
@@ -8,19 +8,168 @@
 // Imports
 //
 
-import { IClue } from './game';
+import {
+    Decoder,
+    object, string, number, array, oneOf, constant, optional,
+} from '@mojotech/json-type-validation';
+
+import { IApiRequest } from '.';
+
+import { IClue, clueDecoder } from './game';
 
 
 //
 // Data model
 //
 
-export interface ISession {
-    id: string, // Unique ID
+export interface IPlayer {
+    uid: string,        // User ID
+    name: string,       // Name
+    avatar: string,     // Avatar
 
-    game: string,       // Base Game ID
-    cols: number,       // Number of columns
-    rows: number,       // Number of rows
-
-    clues: IClue[],    // Clues
+    score: number,      // Game score
+    challenged: number, // Number of challenged question
+    correct: number,    // Number of correct answer
+    wrong: number,      // Number of wrong answer 
 };
+
+export const playerDecoder: Decoder<IPlayer> = object({
+    uid: string(),
+    name: string(),
+    avatar: string(),
+    score: number(),
+    challenged: number(),
+    correct: number(),
+    wrong: number(),
+});
+
+export enum ESessionType {
+    SOLO = 'solo',
+    ONLINE = 'online',
+};
+
+export interface ISession {
+    id: string,             // Unique ID
+
+    title: string,          // Session title
+
+    type: ESessionType,     // Session type
+    game: string,           // Base Game ID
+    cols: number,           // Number of columns
+    rows: number,           // Number of rows
+
+    board: IClue[],         // Clues
+    categories: string[],   // Category strings
+
+    players: IPlayer[],     // Players
+
+    host?: string,          // Host UID
+
+    created: number,        // Creation timestamp
+    started: number,        // Session start timestamp
+    finished: number,       // Session finish timestamp
+};
+
+export const sessionDecoder: Decoder<ISession> = object({
+    id: string(),
+    title: string(),
+    type: oneOf(constant(ESessionType.SOLO), constant(ESessionType.ONLINE)),
+    game: string(),
+    cols: number(),
+    rows: number(),
+    board: array(clueDecoder),
+    categories: array(string()),
+    players: array(playerDecoder),
+    host: optional(string()),
+    created: number(),
+    started: number(),
+    finished: number(),
+});
+
+
+//
+// Api request
+//
+
+export interface ISessionCreateRequest extends IApiRequest {
+    title: string,
+    type: ESessionType,
+    game: string,
+    cols: number,
+    rows: number,
+    board: IClue[],
+    categories: string[],
+    players: IPlayer[],
+};
+
+export const sessionCreateRequestDecoder: Decoder<ISessionCreateRequest> = object({
+    title: string(),
+    type: oneOf(constant(ESessionType.SOLO), constant(ESessionType.ONLINE)),
+    game: string(),
+    cols: number(),
+    rows: number(),
+    board: array(clueDecoder),
+    categories: array(string()),
+    players: array(playerDecoder),
+});
+
+export interface ISessionCreateSoloRequest extends ISessionCreateRequest {
+    type: ESessionType.SOLO,
+};
+
+export const sessionCreateSoloRequestDecoder: Decoder<ISessionCreateSoloRequest> = object({
+    title: string(),
+    type: constant(ESessionType.SOLO),
+    game: string(),
+    cols: number(),
+    rows: number(),
+    board: array(clueDecoder),
+    categories: array(string()),
+    players: array(playerDecoder),
+});
+
+export interface ISessionCreateOnlineRequest extends ISessionCreateRequest {
+    type: ESessionType.ONLINE,
+};
+
+export const sessionCreateOnlineRequestDecoder: Decoder<ISessionCreateOnlineRequest> = object({
+    title: string(),
+    type: constant(ESessionType.ONLINE),
+    game: string(),
+    cols: number(),
+    rows: number(),
+    board: array(clueDecoder),
+    categories: array(string()),
+    players: array(playerDecoder),
+});
+
+export interface ISessionUpdate {
+    title?: string,
+    type?: ESessionType,
+    game?: string,
+    cols?: number,
+    rows?: number,
+    board?: IClue[],
+    categories?: string[],
+    players?: IPlayer[],
+    host?: string,
+    created?: number,
+    started?: number,
+    finished?: number,
+};
+
+export const sessionUpdateDecoder: Decoder<ISessionUpdate> = object({
+    id: optional(string()),
+    title: optional(string()),
+    type: optional(oneOf(constant(ESessionType.SOLO), constant(ESessionType.ONLINE))),
+    game: optional(string()),
+    cols: optional(number()),
+    rows: optional(number()),
+    board: optional(array(clueDecoder)),
+    categories: optional(array(string())),
+    players: optional(array(playerDecoder)),
+    host: optional(optional(string())),
+    created: optional(number()),
+    started: optional(number()),
+    finished: optional(number()),
+});
